a3100254b597e6a031ffc8f97aae6d9fa1bf3841
AustinJ <austinKurtis@users.noreply.github.com>
2018-01-07 00:49:34 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyCAXONNaqeuhpXhsIMLAttE3Fof4Ir2tIk",
-    authDomain: "keepintouc-b6d39.firebaseapp.com",
-    databaseURL: "https://keepintouc-b6d39.firebaseio.com",
-    projectId: "keepintouc-b6d39",
-    storageBucket: "keepintouc-b6d39.appspot.com",
-    messagingSenderId: "900632094808"
-  }
+
 };
